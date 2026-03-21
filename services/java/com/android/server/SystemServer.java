ec5ec782297f1609a920e23cde61810c88d878ca
vvb2060 <vvb2060@gmail.com>
2026-03-21 22:49:49 +0800

    [ims] adding target_ims_override flag
    Change-Id: I2265e691bb7d5214f2e8920afe83844ca19c3237
    Co-authored-by: rmp22 <195054967+rmp22@users.noreply.github.com>

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -187,6 +187,7 @@ import com.android.server.grammaticalinflection.GrammaticalInflectionService;
 import com.android.server.graphics.fonts.FontManagerService;
 import com.android.server.hdmi.HdmiControlService;
 import com.android.server.incident.IncidentCompanionService;
+import com.android.server.ims.ImsConfigOverrideService;
 import com.android.server.input.InputManagerService;
 import com.android.server.inputmethod.InputMethodManagerService;
 import com.android.server.integrity.AppIntegrityManagerService;
@@ -2908,6 +2909,11 @@ public final class SystemServer implements Dumpable {
                 t.traceBegin("StartBackgroundInstallControlService");
                 mSystemServiceManager.startService(BackgroundInstallControlService.class);
                 t.traceEnd();
+            }
+
+            if (SystemProperties.getBoolean(
+                    "persist.sys.target_enables_ims_override", false)) {
+                mSystemServiceManager.startService(ImsConfigOverrideService.class);
             }
         }
 
