b5a08e51c36e77a05e92bcf13bf9e65c9cfa0676
rmp22 <195054967+rmp22@users.noreply.github.com>
2026-04-03 22:34:57 +0800

    [game_space] adding immersive lock gesture
    Change-Id: I219bc7b9aac04cccab299c7ac940d1ebad10593e

--- services/core/java/com/android/server/wm/DisplayPolicy.java
+++ services/core/java/com/android/server/wm/DisplayPolicy.java
@@ -419,6 +419,7 @@ public class DisplayPolicy {
 
     private volatile boolean mAxPcModeEnabled;
     private volatile int mAxPcModeTargetDisplayId;
+    private volatile boolean mGamingGestureLocked;
 
     private class PolicyHandler extends Handler {
 
@@ -497,7 +498,7 @@ public class DisplayPolicy {
 
                 @Override
                 public void onSwipeFromTop() {
-                    if (isAxPcModeEnabled()) {
+                    if (isAxPcModeEnabled() || isGamingGestureLocked()) {
                         return;
                     }
                     synchronized (mLock) {
@@ -508,7 +509,7 @@ public class DisplayPolicy {
 
                 @Override
                 public void onSwipeFromBottom() {
-                    if (isAxPcModeEnabled()) {
+                    if (isAxPcModeEnabled() || isGamingGestureLocked()) {
                         return;
                     }
                     synchronized (mLock) {
@@ -518,7 +519,7 @@ public class DisplayPolicy {
                 }
 
                 private boolean allowsSideSwipe(Region excludedRegion) {
-                    if (isAxPcModeEnabled()) {
+                    if (isAxPcModeEnabled() || isGamingGestureLocked()) {
                         return false;
                     }
                     return mNavigationBarAlwaysShowOnSideGesture
@@ -527,7 +528,7 @@ public class DisplayPolicy {
 
                 @Override
                 public void onSwipeFromRight() {
-                    if (isAxPcModeEnabled()) {
+                    if (isAxPcModeEnabled() || isGamingGestureLocked()) {
                         return;
                     }
                     final Region excludedRegion = Region.obtain();
@@ -545,7 +546,7 @@ public class DisplayPolicy {
 
                 @Override
                 public void onSwipeFromLeft() {
-                    if (isAxPcModeEnabled()) {
+                    if (isAxPcModeEnabled() || isGamingGestureLocked()) {
                         return;
                     }
                     final Region excludedRegion = Region.obtain();
@@ -765,6 +766,19 @@ public class DisplayPolicy {
                 "ax_pc_mode"), false, pcModeObserver, UserHandle.USER_ALL);
         resolver.registerContentObserver(Settings.Secure.getUriFor(
                 "ax_pc_mode_target_display_id"), false, pcModeObserver, UserHandle.USER_ALL);
+
+        mGamingGestureLocked = Settings.Secure.getIntForUser(resolver,
+                "ax_gaming_gesture_lock", 0, UserHandle.USER_CURRENT) == 1;
+        ContentObserver gestureLockObserver = new ContentObserver(mHandler) {
+            @Override
+            public void onChange(boolean selfChange) {
+                mGamingGestureLocked = Settings.Secure.getIntForUser(
+                        mContext.getContentResolver(),
+                        "ax_gaming_gesture_lock", 0, UserHandle.USER_CURRENT) == 1;
+            }
+        };
+        resolver.registerContentObserver(Settings.Secure.getUriFor(
+                "ax_gaming_gesture_lock"), false, gestureLockObserver, UserHandle.USER_ALL);
     }
 
     private void updateForceShowNavBarSettings() {
@@ -829,6 +843,10 @@ public class DisplayPolicy {
 
     public int getDockMode() {
         return mDockMode;
+    }
+
+    public boolean isGamingGestureLocked() {
+        return mGamingGestureLocked;
     }
 
     public boolean isAxPcModeEnabled() {
@@ -2483,7 +2501,7 @@ public class DisplayPolicy {
         if (CLIENT_TRANSIENT) {
             return;
         }
-        if (isAxPcModeEnabled()) {
+        if (isAxPcModeEnabled() || isGamingGestureLocked()) {
             return;
         }
         if (swipeTarget == null || !mService.mPolicy.isUserSetupComplete()) {
